6d61e773831e532e8876487403db9c79d2408294
ssnepenthe <ssnepenthe@users.noreply.github.com>
2017-05-06 14:42:57 -0700

    Update the way http test fixtures are retrieved

--- tests/class-filesystem-cache.php
+++ tests/class-filesystem-cache.php
@@ -4,13 +4,11 @@ class Filesystem_Cache implements Soter_Core\Cache_Interface {
 	use Gets_Http_Fixtures_Trait;
 
 	public function contains( $key ) {
-		return $this->http_fixture_exists( $this->get_file( $key ) );
+		return static::http_fixture_exists( $this->get_file( $key ) );
 	}
 
 	public function fetch( $key ) {
-		return static::get_http_fixture_array(
-			$this->get_http_fixture_path( $this->get_file( $key ) )
-		);
+		return static::get_http_fixture_array( $this->get_file( $key ) );
 	}
 
 	public function save( $key, $data, $lifetime ) {
--- tests/class-filesystem-http-client.php
+++ tests/class-filesystem-http-client.php
@@ -6,8 +6,6 @@ class Filesystem_Http_Client implements Soter_Core\Http_Interface {
 	public function get( $url ) {
 		$parsed = parse_url( $url );
 
-		$fixture_path = $this->get_http_fixture_path( $parsed['path'] );
-
-		return static::get_http_fixture_array( $fixture_path );
+		return static::get_http_fixture_array( $parsed['path'] );
 	}
 }
